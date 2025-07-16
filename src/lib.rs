90ca53e5a194b152a266a67fe201c806cf26d34b
* <obitodih@gmail.com>
2025-07-16 23:03:23 +0300

    Some Add

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
+//! # nerdicons-rs
+//! Easily use Nerd Font icons in Rust.
+//!
+//! ## Example
+//! ```rust
+//! use nerdicons_rs::linux::RSAPPLE;
+//! fn main() {
+//!     println!("{}", RSAPPLE);
+//! }
+//! ```
 pub mod icons;
 pub use icons::*;
